edbe27663dba998a4ff5d6e8c8ac0dd50293f24c
(s) Jevan Duffy <jduffy2@pm7-0102.uopnet.plymouth.ac.uk>
2018-11-14 11:40:31 +0000

    code update

--- 00_Template/sketch.js
+++ 00_Template/sketch.js
@@ -2,22 +2,39 @@
 //00_Template
 //The purpose of this sketch is to provide a basic template
 //for P5.js projects
-
+var  angle = 0;
 //Initialization function
 function setup() {
   //Print a message to the console - to view using Chrome:
   //View > Developer > Developer Tools > Console
-  console.log("Initialization: OK")
-  createCanvas(500,500);
+
+  createCanvas(710,500);
+  //Set a background color
+  background(200, 0, 255);
+  fill(0,255,0);
 }
 
 //Rendering function
 function draw() {
-  //Set a background color
-  background(255, 0, 255);
 
+    // Draw only when mouse is pressed
+    //when mouse is pressed draws
+  if (mouseIsPressed == true) {
+    angle += 5;
+
+    var val = cos(radians(angle)) * 30.0;
+    for (var a = 0; a < 360; a += 75) {
+      var xoff = cos(radians(a)) * val;
+      var yoff = sin(radians(a)) * val;
+      fill(random(255),random(255) ,random(255));
+      ellipse(mouseX + xoff, mouseY + yoff, val, val);
+    }
+    //
+    fill(255,0,0);
+    ellipse(mouseX, mouseY, 2, 3)
+  }
   //Print a message to the console - to view using Chrome:
   //View > Developer > Developer Tools > Console
-  console.log("Rendering...")
-  noLoop();
+
+
 }
